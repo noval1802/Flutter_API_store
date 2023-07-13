735b8730d8b12af51a3716ae6dc3dc237fe03284
noval1802 <abdulazizkon@gmail.com>
2023-07-14 02:28:03 +0700

    submit

---
+++ Flutter_Aplication_API_store/lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_grocery_ui/cubit/product_cubit.dart';
+import 'package:flutter_grocery_ui/item_widget.dart';
+
+import 'data.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ProductCubit()..getProducts(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MainPage(),
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  // final data = Product(
+  //   'Bayam',
+  //   '2.000',
+  //   '1ikat',
+  //   'assets/img1.png',
+  //   'Secara umum sayuran dan buah-buahan merupakan sumber berbagai vitamin, mineral, dan serat pangan. Sebagian vitamin dan mineral yang terkandung dalam sayuran dan buah-buahan berperan untuk membantu proses-proses metabolisme di dalam tubuh, sedangkan antioksidan mampu menangkal senyawa-senyawa hasil oksidasi, radikal bebas, yang mampu menurunkan kondisi kesehatan tubuh',
+  // );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Color.fromARGB(255, 100, 204, 197),
+        leading: const Icon(
+          Icons.menu,
+          color: Colors.black,
+        ),
+        title: const Text(
+          'Online Store',
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          Row(
+            children: [
+              const Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              Stack(
+                children: [
+                  IconButton(
+                    onPressed: (() {}),
+                    icon: const Icon(
+                      Icons.shopping_cart,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Positioned(
+                    top: 0,
+                    right: 3,
+                    child: Container(
+                      height: 20,
+                      width: 20,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(255, 100, 204, 197),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "2",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 65, 14, 14),
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+      body: BlocBuilder<ProductCubit, ProductState>(
+        builder: (context, state) {
+          if (state is ProductLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is ProductSuccess) {
+            return Container(
+              padding: const EdgeInsets.all(10),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.65,
+                ),
+                itemBuilder: (context, index) {
+                  return ItemWidget(product: state.products[index]);
+                },
+                itemCount: state.products.length,
+              ),
+            );
+          }
+          return const Center(
+            child: Text('No Data'),
+          );
+        },
+      ),
+    );
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Flutter_API_store
+
+* ```git clone https://github.com/noval1802/Flutter_API_store.git```
+* ```flutter pub get```
+* ```flutter run -d edge ``` / ```flutter run```
+
+## Run the app in debug mode android.
+![menu product](assets/menu_product.png)
+
+### Detail product
+![detail_product](assets/detail_product.png)
